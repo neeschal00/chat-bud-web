aa574cc790a13bc11be0fa61778d8f894f289b87
Nischal Khatri <nischal06khatri@gmail.com>
2022-01-24 22:46:52 +0545

    Conditional rendering with addition of sidebar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,54 @@ import {ThemeProvider, theme, ColorModeProvider,CSSReset} from '@chakra-ui/react
 import NavBar from './Components/NavBar';
 import Footer from './Components/Footer';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 import Routesl from './Routes'
+import SideBar from './Components/SideBar';
+
+
+const Header = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  console.log(location);
+  console.log(location.pathname);
+  if (path === "/") {
+    return (
+        <SideBar />
+      );
+    }
+    else {
+      return (
+        <NavBar />
+    );
+  }
+}
+
+const Foot = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  console.log(location);
+  console.log(location.pathname);
+  if (path === "/") {
+    return (
+      <>  </>
+    );
+  }
+  else {
+    return (
+      <Footer />
+    );
+  }
+}
+
 function App() {
+  
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <div className="App"> 
-            <NavBar />
+            <Header />
             <Routesl />
-            <Footer />
+            <Foot />
         </div>
       </Router>
       
