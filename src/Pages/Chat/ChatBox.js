641e21cf539be9bc1102becb47f33e5d95d40f22
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-07 23:32:39 +0545

    Working on the chat User Interface

--- src/Pages/Chat/ChatBox.js
+++ src/Pages/Chat/ChatBox.js
@@ -3,24 +3,46 @@ import { Avatar,
     Container,
     Flex,
     propNames,
+    FormErrorMessage,
+    FormControl,
+    FormLabel,
     Text,
+    Textarea,
     Spacer,
-    Input,
-    InputGroup,
-    InputRightElement,
-    Button, 
-    Textarea} from "@chakra-ui/react";
+    HStack,
+    } from "@chakra-ui/react";
+
+    import * as yup from 'yup';
+
     import {
-        InputControl,
+        TextareaControl,
         SubmitButton,
       } from "formik-chakra-ui";
+
       import { Formik, Field, Form, useField } from 'formik';
+
 import { useState } from "react";
 import { useColorModeValue } from "@chakra-ui/react";
 import {FiSend} from 'react-icons/fi';
 const ChatBox = ({chatId,chatImage,chatName}) => {
     console.log("chatbox",chatId);
-    const [chatMessages, setChatMessages] = useState([{},{}]);
+    const [chatMessages, setChatMessages] = useState([
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nischay",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        }
+]);
     const [messagevalue,setMessageValue ]= useState("");
     let color = useColorModeValue("black","white");
     let borderColor = useColorModeValue("#A0AEC0","#CBD5E0")
@@ -33,7 +55,7 @@ const ChatBox = ({chatId,chatImage,chatName}) => {
                     <Spacer />
                     
                 </Flex>
-                
+            
             </Box>
             <Box overflowY="auto"
                 h="96"
@@ -49,29 +71,77 @@ const ChatBox = ({chatId,chatImage,chatName}) => {
                     borderRadius: '24px',
                     },
                 }}>
+
             </Box>
             <Box pos="-webkit-sticky">
 
                 
-                    <Textarea placeholder="Type a message" resize="none" value={messagevalue} onChange={(event) =>setMessageValue(event.target.value)} />
-                    <InputRightElement h={'full'}>
-                        <Button
-                        variant={'ghost'}
-                        rightIcon={<FiSend />}
-
-                        isLoading= {false}
-                        _hover={{ bg: '#553C9A' }}
-                        bg={"#805AD5"}
-                        onClick={() =>{
-                            
-                            console.log('Searching for: ', messagevalue)
-                            
-                            }}>
-                        </Button>
-                    </InputRightElement>
+                    <ChatInputForm />
                 
             </Box>
         </Box>
     );
 }
+
+const TextAreaField = ({ label,...props }) => {
+    const [field, meta] = useField(props)
+    const errorText = meta.error && meta.touched ? meta.error : ''
+    
+    return (
+        <FormControl id="message" isRequired isInvalid={!!meta.error && meta.touched} >
+            <Field as={Textarea} {...field} {...props} />
+            {!!errorText && <FormErrorMessage>{errorText}</FormErrorMessage>}
+        </FormControl>
+    )
+}
+
+const validationSchema = yup.object({
+    message: yup.string().required('Message is required'),
+        
+  })
+const ChatInputForm = (props) =>{
+    const iborder = useColorModeValue("gray.400","gray.200");
+
+    return(
+        <Formik
+        initialValues={{  
+          message: ''}}
+        validationSchema={validationSchema}
+        validateOnChange = {false}
+        validateOnBlur = {false}
+        validateOnMount = {false}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          console.log(values);
+          setSubmitting(false);
+          resetForm();
+        } }
+        >
+        {({ values,setFieldValue,errors,tuched,isSubmitting, isValid, handleSubmit }) => (
+        <Form>
+          <HStack spacing={2}>
+          <TextAreaField placeholder="Type a message" resize="none" name="message" border='1px' borderColor={iborder} borderRadius="md" />
+                <SubmitButton
+                  type="submit"
+                  loadingText="Submitting"
+                  isLoading={isSubmitting}
+                  isDisabled={!isValid}
+                  rightIcon={<FiSend></FiSend>}
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Send
+                </SubmitButton>
+            </HStack>
+        </Form>
+        )}
+      </Formik>
+    )
+}
+
+
+
+
 export default ChatBox;
--- src/Pages/Chat/ChatDetails.js
+++ src/Pages/Chat/ChatDetails.js
@@ -1,4 +1,4 @@
-import { Box,Container,Flex,Text } from "@chakra-ui/react";
+import { Avatar, Box,Container,Flex,Text, VStack,Center,useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
 
@@ -6,13 +6,24 @@ import React from "react";
 const ChatDetails =(props) =>{
     console.log("details",props.chatId);
     const value = props.chatId;
+    const borderColor = useColorModeValue("gray.400","gray.200");
     return (
-        <Box h="full">
-            <Container p={4}>
-                <Flex>
-                    <Text>hellow {value}</Text>
-                </Flex>
-            </Container>
+        <Box h="full" w="40%" borderLeft="1px" borderColor={borderColor}>
+            
+            <VStack spacing="1.5" mt='1.5' alignItems="center" justifyContent="center">
+                <Center>
+
+                    <Box>
+                        <Avatar size="2xl" src={props.chatImage} />
+                    </Box>
+                </Center>
+                <Center>
+                    <Box>
+                        <Text fontWeight="bold" fontSize="2xl">{props.chatName}</Text>
+                    </Box>
+                </Center>
+            </VStack>
+    
         </Box>
     );
 }
