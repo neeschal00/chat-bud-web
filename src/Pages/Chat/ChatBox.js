1781095a0034f050072eeb60c4ff75dff133a600
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-10 20:08:17 +0545

    Fixed chat bubble

--- src/Pages/Chat/ChatBox.js
+++ src/Pages/Chat/ChatBox.js
@@ -10,22 +10,15 @@ import { Avatar,
     Textarea,
     Spacer,
     HStack,
+    useColorModeValue,
 
     } from "@chakra-ui/react";
 
-    import * as yup from 'yup';
     
 
-    import {
-        TextareaControl,
-        SubmitButton,
-      } from "formik-chakra-ui";
+import { useEffect, useState,useRef } from "react";
 
-      import { Formik, Field, Form, useField } from 'formik';
-
-import { useEffect, useState } from "react";
-import { useColorModeValue } from "@chakra-ui/react";
-import {FiSend} from 'react-icons/fi';
+import ChatInputForm from "./ChatInputForm";
 const ChatBox = ({chatId,chatImage,chatName,chatType}) => {
     console.log("chatbox",chatId);
     const [chatMessages, setChatMessages] = useState([
@@ -43,11 +36,128 @@ const ChatBox = ({chatId,chatImage,chatName,chatType}) => {
             createdAt: { type: Date, default: Date.now },
             updatedAt: { type: Date, default: Date.now },
         
+        },
+        {
+            chatId: chatId,
+            message: "nishan hello",
+            isSent: true,
+            senderId: "nishan",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        },
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nischay",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        },
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nishan",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        },
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nishan",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        },
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nischay",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        },
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nishan",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
+        },
+        {
+            chatId: chatId,
+            message: "Hello",
+            isSent: true,
+            senderId: "nischay",
+            senderName: "Nishan bhatey",
+            senderAvatar: "https://images.unsplash.com/photo-1593642647962-b9e8a0c4f7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            senderIsDeleted: false,
+            senderIsBlocked: { type: Boolean, required: true },
+            senderIsAdmin: { type: Boolean, required: true },
+            senderIsActive: { type: Boolean, required: true },
+            createdAt: { type: Date, default: Date.now },
+            updatedAt: { type: Date, default: Date.now },
+        
         }
+
+
 ]);
     const [messagevalue,setMessageValue ]= useState("");
     let color = useColorModeValue("black","white");
-    let borderColor = useColorModeValue("#A0AEC0","#CBD5E0")
+    let borderColor = useColorModeValue("#A0AEC0","#CBD5E0");
+
+    const messagesEndRef = useRef(null)
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, []);
     return(
         <Box  height="full" width={{base:"100%",md:"60%",lg:"60%"}}>
             <Box pos="sticky" h="14"  borderBottom="1px" borderColor={borderColor}>
@@ -63,6 +173,7 @@ const ChatBox = ({chatId,chatImage,chatName,chatType}) => {
                 h="96"
                 display="flex"
                 flexDirection="column"
+                
                 css={{
                     '&::-webkit-scrollbar': {
                     width: '4px',
@@ -76,57 +187,10 @@ const ChatBox = ({chatId,chatImage,chatName,chatType}) => {
                     },
                 }}>
                     
-
-                <Box  display="flex" flexDirection="column">
-                    <Box display="flex" justifyContent="flex-start">
-                        <Flex>
-
-                            <Avatar size="md" name="John Doe" src={chatImage} />
-                            <Box maxWidth="60" bgColor="blue.300" borderRadius="sm">
-                                { (chatType === "group")? <Text fontSize="md" fontWeight="bold" color={color} ml="1.5">{chatName}</Text>:null}
-                                <Text overflowWrap="break-word" fontSize="md" fontWeight="normal" color={color} ml="1.5">helloo mgfjhfgnv yufcudckudcyudckyjucuycuycyucyuuytgdtudtttttttttttttttttttttttttttttttttttttttt</Text>
-                            </Box>
-                        </Flex>
-                    </Box>
-                </Box>
-                <Box  display="flex" flexDirection="column">
-                    <Box display="flex" justifyContent="flex-end" mr="1.5">
-                        <Flex>
-
-                            <Avatar size="md" name="John Doe" src={chatImage} />
-                            <Box maxWidth="60">
-                                { (chatType === "group")? <Text fontSize="md" fontWeight="bold" color={color}>{chatName}</Text>:null}
-                                <Text overflowWrap="break-word" fontSize="md" fontWeight="normal" color={color}>helloo mgfjhfgnv yufcudckudcyudckyjucuycuycyucyuuytgdtudtttttttttttttttttttttttttttttttttttttttt</Text>
-                            </Box>
-                        </Flex>
-                    </Box>
-                </Box>
-
-                <Box  display="flex" flexDirection="column">
-                    <Box display="flex" justifyContent="flex-start">
-                        <Flex>
-
-                            <Avatar size="md" name="John Doe" src={chatImage} />
-                            <Box maxWidth="60" bgColor="blue.300" borderRadius="sm">
-                                { (chatType === "group")? <Text fontSize="md" fontWeight="bold" color={color} ml="1.5">{chatName}</Text>:null}
-                                <Text overflowWrap="break-word" fontSize="md" fontWeight="normal" color={color} ml="1.5">helloo mgfjhfgnv yufcudckudcyudckyjucuycuycyucyuuytgdtudtttttttttttttttttttttttttttttttttttttttt</Text>
-                            </Box>
-                        </Flex>
-                    </Box>
-                </Box>
-                <Box  display="flex" flexDirection="column">
-                    <Box display="flex" justifyContent="flex-end" mr="1.5">
-                        <Flex>
-
-                            <Avatar size="md" name="John Doe" src={chatImage} />
-                            <Box maxWidth="60">
-                                { (chatType === "group")? <Text fontSize="md" fontWeight="bold" color={color}>{chatName}</Text>:null}
-                                <Text overflowWrap="break-word" fontSize="md" fontWeight="normal" color={color}>helloo mgfjhfgnv yufcudckudcyudckyjucuycuycyucyuuytgdtudtttttttttttttttttttttttttttttttttttttttt</Text>
-                            </Box>
-                        </Flex>
-                    </Box>
-                </Box>
-
+                {chatMessages.map((message,index) => (<ChatBubble key={index} message={message} />))}
+                
+            
+                <div ref={messagesEndRef}/>
 
             </Box>
             <Box pos="-webkit-sticky">
@@ -139,63 +203,40 @@ const ChatBox = ({chatId,chatImage,chatName,chatType}) => {
     );
 }
 
-const TextAreaField = ({ label,...props }) => {
-    const [field, meta] = useField(props)
-    const errorText = meta.error && meta.touched ? meta.error : ''
+const ChatBubble = ({message}) => {
+    console.log(message);
     
+    if (message.senderId === "nischay") {
+        return (
+            <Box  display="flex" flexDirection="column">
+                <Box display="flex" justifyContent="flex-end" mr="1.5">
+                    <Flex>
+                        <Box maxWidth="60" bgColor="#8774E1" borderRadius="md" p="1.5" marginBottom="1.5" marginTop="0.5">
+                            { (message.chatType === "group")? <Text fontSize="md" fontWeight="bold">{message.chatName}</Text>:null}
+                            <Text overflowWrap="break-word" fontSize="md" fontWeight="normal">helloo mgfjhfgnv yufcudckudcyudckyjucuycuycyucyuuytgdtudtttttttttttttttttttttttttttttttttttttttt</Text>
+                        </Box>
+                    </Flex>
+                </Box>
+            </Box>
+        );
+    }
     return (
-        <FormControl id="message" isRequired isInvalid={!!meta.error && meta.touched} >
-            <Field as={Textarea} {...field} {...props} />
-            {!!errorText && <FormErrorMessage>{errorText}</FormErrorMessage>}
-        </FormControl>
-    )
+        <Box  display="flex" flexDirection="column">
+                    <Box display="flex" justifyContent="flex-start">
+                        <Flex>
+                            <Avatar size="md" name="John Doe" src={message.chatImage} />
+                            <Box maxWidth="60" bgColor="#8774E1" borderRadius="md" p="1.5" marginBottom="1.5">
+                                { (message.chatType === "group")? <Text fontSize="md" fontWeight="bold"  ml="1.5">{message.chatName}</Text>:null}
+                                <Text overflowWrap="break-word" fontSize="md" fontWeight="normal"  ml="1.5">helloo mgfjhfgnv yufcudckudcyudckyjucuycuycyucyuuytgdtudtttttttttttttttttttttttttttttttttttttttt</Text>
+                            </Box>
+                        </Flex>
+                    </Box>
+                </Box>
+    );
+
 }
 
-const validationSchema = yup.object({
-    message: yup.string().required('Message is required'),
-        
-  })
-const ChatInputForm = (props) =>{
-    const iborder = useColorModeValue("gray.400","gray.200");
 
-    return(
-        <Formik
-        initialValues={{  
-          message: ''}}
-        validationSchema={validationSchema}
-        validateOnChange = {false}
-        validateOnBlur = {false}
-        validateOnMount = {false}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          setSubmitting(true);
-          console.log(values);
-          setSubmitting(false);
-          resetForm();
-        } }
-        >
-        {({ values,setFieldValue,errors,tuched,isSubmitting, isValid, handleSubmit }) => (
-        <Form>
-          <HStack spacing={2}>
-          <TextAreaField placeholder="Type a message" resize="none" name="message" border='1px' borderColor={iborder} borderRadius="md" />
-                <SubmitButton
-                  type="submit"
-                  loadingText="Submitting"
-                  isLoading={isSubmitting}
-                  isDisabled={!isValid}
-                  rightIcon={<FiSend></FiSend>}
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Send
-                </SubmitButton>
-            </HStack>
-        </Form>
-        )}
-      </Formik>
-    )
-}
 
 
 
---
+++ src/Pages/Chat/ChatInputForm.js
@@ -0,0 +1,73 @@
+import * as yup from 'yup';
+    
+
+    import {
+        TextareaControl,
+        SubmitButton,
+      } from "formik-chakra-ui";
+
+      import { Formik, Field, Form, useField } from 'formik';
+import {useState,useEffect} from 'react';
+import { HStack,FormControl, Textarea, FormErrorMessage, useColorModeValue } from '@chakra-ui/react';
+import {FiSend} from 'react-icons/fi';
+
+const TextAreaField = ({ label,...props }) => {
+    const [field, meta] = useField(props)
+    const errorText = meta.error && meta.touched ? meta.error : ''
+    
+    return (
+        <FormControl id="message" isRequired isInvalid={!!meta.error && meta.touched} >
+            <Field as={Textarea} {...field} {...props} />
+            {!!errorText && <FormErrorMessage>{errorText}</FormErrorMessage>}
+        </FormControl>
+    )
+}
+
+const validationSchema = yup.object({
+    message: yup.string().required('Message is required'),
+        
+  })
+const ChatInputForm = (props) =>{
+    const iborder = useColorModeValue("gray.400","gray.200");
+
+    return(
+        <Formik
+        initialValues={{  
+          message: ''}}
+        validationSchema={validationSchema}
+        validateOnChange = {false}
+        validateOnBlur = {false}
+        validateOnMount = {false}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          console.log(values);
+          setSubmitting(false);
+          resetForm();
+        } }
+        >
+        {({ values,setFieldValue,errors,tuched,isSubmitting, isValid, handleSubmit }) => (
+        <Form>
+          <HStack spacing={2}>
+          <TextAreaField placeholder="Type a message" resize="none" name="message" border='1px' borderColor={iborder} borderRadius="md" />
+                <SubmitButton
+                  type="submit"
+                  loadingText="Submitting"
+                  isLoading={isSubmitting}
+                  isDisabled={!isValid}
+                  rightIcon={<FiSend></FiSend>}
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Send
+                </SubmitButton>
+            </HStack>
+        </Form>
+        )}
+      </Formik>
+    )
+}
+
+
+export default ChatInputForm;
