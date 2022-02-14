ffef1d8901657b0912b28b46c7abbbefc211c8d1
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-14 22:42:55 +0545

    Added 404 not found route and component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -100,7 +100,7 @@ export default function Footer() {
           spacing={4}
           justify={{ md: 'space-between' }}
           align={{ md: 'center' }}>
-          <Text>© 2020 Chakra Templates. All rights reserved</Text>
+          <Text>© Chat Bud. All rights reserved</Text>
           <Stack direction={'row'} spacing={6}>
             <SocialButton label={'Twitter'} href={'#'}>
               <FaTwitter />
--- src/Components/GenericNotFound.js
+++ src/Components/GenericNotFound.js
@@ -1,12 +1,12 @@
 import Lottie from "lottie-react";
-import {Box,Container, Flex,Text} from '@chakra-ui/core';
-import error from '../Lottie/404error.json';
+import {Box,Container, Flex,Text} from '@chakra-ui/react';
+import error from '../Lottie/404notfound.json';
 const GenericNotFound = () => {
     return (
         <Container>
-            <Box height={"100%" }>
-            <Text fontSize='6xl' fontWeight="bold">404 Not Found</Text>
-            <Lottie animationData={error} />
+            <Box height="96">
+                {/* <Lottie options={{animationData:error}} height={'100%'} width={'100%'}/> */}
+                <Lottie animationData={error} height={"100%"} width={"90%"} />
             </Box>
         </Container>
     )
